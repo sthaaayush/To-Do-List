f974cca7722b8adaa91e840e6fc45df95a547ac4
Aayush Shrestha <aayushstha20@gmail.com>
2025-07-21 12:38:56 +0545

    Basic Layout of Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './MyComponents/Header';
+import Todos from './MyComponents/Todos';
+import Footer from './MyComponents/Footer';
 
 function App() {
   return (
     <>
       <Header/>
+      <ToDos/>
+      <Footer/>
     </>
   );
 }
