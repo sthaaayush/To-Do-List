07f5818c3b782e3aba1eaddc680ec9885765e3c8
Aayush Shrestha <aayushstha20@gmail.com>
2025-07-21 14:52:27 +0545

    Footer: Added Basic Footer

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,3 +1,13 @@
 import React from "react";
 
-
+export default function Footer() {
+  return (
+    <>
+      <footer className="bg-dark text-light py-3 text-center" style={
+        { position: "fixed", top: "90vh", width: "100%" }
+      }>
+        <p> Copyright &copy; 2023 TodoList App</p>
+      </footer>
+    </>
+  );
+}
