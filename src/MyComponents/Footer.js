fb29aeb826f051a33d9be3395f48f948773fd8ca
Aayush Shrestha <aayushstha20@gmail.com>
2025-07-23 18:10:38 +0545

    Footer: Updated footer

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,15 +1,34 @@
-export default function Footer() {
-  let footerStyle = {
-    position: "fixed",
-    left: 0,
-    bottom: 0,
-    width: "100%",
-    marginTop: "5em",
-    zIndex: 1000
-  }
-  return (
-    <footer className="bg-dark text-light py-3 text-center " style={footerStyle}>
-      <p> Copyright &copy; 2023 TodoList App</p>
-    </footer>
-  );
-}
+import React from "react";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+const Footer = () => {
+    const footerStyle = {
+        position: "relative",
+        left: 0,
+        top: "50vh",
+        width: "100%",
+        zIndex: 100,
+    };
+    return (
+        <footer className="bg-dark text-white text-center py-3 mt-5" style={footerStyle}>
+            <div className="container">
+                <div className="mb-2">
+                    &copy; {new Date().getFullYear()} Your Company. All rights reserved.
+                </div>
+                <div>
+                    <a href="https://facebook.com" className="text-white mx-2" target="_blank" rel="noopener noreferrer">
+                        <i className="bi bi-facebook"></i>
+                    </a>
+                    <a href="https://github.com" className="text-white mx-2" target="_blank" rel="noopener noreferrer">
+                        <i className="bi bi-github"></i>
+                    </a>
+                    <a href="https://linkedin.com" className="text-white mx-2" target="_blank" rel="noopener noreferrer">
+                        <i className="bi bi-linkedin"></i>
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
